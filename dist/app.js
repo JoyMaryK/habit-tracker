361c99a195b25070e4edd616361a27863d6eef37
Joy Mary <jmkirongo@gmail.com>
2023-04-28 20:21:16 +0300

    makes further ui fixes

--- dist/app.js
+++ dist/app.js
@@ -13,19 +13,17 @@ class Habit {
     addHabit(event) {
         return __awaiter(this, void 0, void 0, function* () {
             event.preventDefault();
-            // console.log("I'm in")
             const habitInput = document.querySelector("#habit-name");
             const habitInputValue = habitInput.value;
             const habitStartDateInput = document.querySelector("#start-date");
             const habitStartDateValue = habitStartDateInput.value;
             const addedHabit = { "habitName": habitInputValue, "startDate": habitStartDateValue };
-            console.log(addedHabit);
-            yield fetch(' http://localhost:3000/habit', {
-                method: 'POST',
+            yield fetch(" http://localhost:3000/habit", {
+                method: "POST",
                 body: JSON.stringify(addedHabit),
                 headers: {
-                    "Content-Type": "application/json"
-                }
+                    "Content-Type": "application/json",
+                },
             });
         });
     }
@@ -33,14 +31,12 @@ class Habit {
         return __awaiter(this, void 0, void 0, function* () {
             const response = yield fetch("http://localhost:3000/habit");
             const allHabits = yield response.json();
-            // console.log(carts);
             let html = ``;
             allHabits.forEach((item) => {
                 let startdate = new Date(item.startDate);
                 let date2 = new Date();
                 let diff = date2.getTime() - startdate.getTime();
                 let diffDays = Math.ceil(diff / (1000 * 3600 * 24));
-                // console.log(diffDays);
                 let htmlItem = `<div class="habit">
         <h3 class="habit-name">${item.habitName}</h3>
         <p>Start Date: ${item.startDate}</p>
@@ -57,4 +53,3 @@ const habit = new Habit();
 const addBt = document.querySelector("#add-habit");
 addBt.addEventListener("click", habit.addHabit);
 habit.getHabits();
-//console.log( Date());
--- src/app.ts
+++ src/app.ts
@@ -1,67 +1,51 @@
-class Habit{
-    constructor(){}
+class Habit {
+  constructor() {}
 
-     async addHabit(event: Event) {
-      event.preventDefault()
-     // console.log("I'm in")
-      const habitInput =  document.querySelector("#habit-name") as HTMLInputElement
-      const habitInputValue = habitInput.value
-      const habitStartDateInput =  document.querySelector("#start-date") as HTMLInputElement
-      const habitStartDateValue = habitStartDateInput.value 
-      
+  async addHabit(event: Event) {
+    event.preventDefault();
 
-      const addedHabit:{} = {  "habitName" : habitInputValue, "startDate": habitStartDateValue}
-    console.log(addedHabit);
-    
-        await fetch(' http://localhost:3000/habit', {
-            method:'POST',
-            body:JSON.stringify(addedHabit),
-            headers:{
-                "Content-Type": "application/json"
-            }
-        })
+    const habitInput = document.querySelector("#habit-name") as HTMLInputElement;
+    const habitInputValue = habitInput.value;
+    const habitStartDateInput = document.querySelector("#start-date") as HTMLInputElement;
+    const habitStartDateValue = habitStartDateInput.value;
+
+    const addedHabit: {} = {"habitName": habitInputValue,  "startDate": habitStartDateValue};
+
+    await fetch(" http://localhost:3000/habit", {
+      method: "POST",
+      body: JSON.stringify(addedHabit),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
   }
 
-  async  getHabits(){
-    const response = await fetch("http://localhost:3000/habit")
-    const allHabits = await response.json()
-   // console.log(carts);
+  async getHabits() {
+    const response = await fetch("http://localhost:3000/habit");
+    const allHabits = await response.json();
 
-  
+    let html = ``;
+    allHabits.forEach((item: { startDate: string; habitName: string }) => {
+      let startdate = new Date(item.startDate);
+      let date2 = new Date();
+      let diff = date2.getTime() - startdate.getTime();
+      let diffDays = Math.ceil(diff / (1000 * 3600 * 24));
 
-   let html = ``
-    allHabits.forEach((item: {
-
-      startDate: string, habitName: string 
-}) => {
-
-  let startdate = new Date(item.startDate)
-  let date2 = new Date ()  
-  let diff = date2.getTime() - startdate.getTime()
-  let diffDays = Math.ceil(diff / (1000 * 3600 * 24));
- // console.log(diffDays);
-  
-        let htmlItem = `<div class="habit">
+      let htmlItem = `<div class="habit">
         <h3 class="habit-name">${item.habitName}</h3>
         <p>Start Date: ${item.startDate}</p>
         <p>Days: ${diffDays}</p>
-     </div>`
-    html+=htmlItem
+     </div>`;
+      html += htmlItem;
     });
-    const habitsRender = document.querySelector(".habits") as HTMLDivElement
-    
-    habitsRender.innerHTML = html
+    const habitsRender = document.querySelector(".habits") as HTMLDivElement;
 
+    habitsRender.innerHTML = html;
+  }
 }
 
+const habit = new Habit();
+const addBt = document.querySelector("#add-habit") as HTMLButtonElement;
+addBt.addEventListener("click", habit.addHabit);
 
-}
-
-const habit = new Habit()
-const addBt = document.querySelector("#add-habit") as HTMLButtonElement
-addBt.addEventListener("click",habit.addHabit)
-
- habit.getHabits()
-
-//console.log( Date());
- 
+habit.getHabits();
